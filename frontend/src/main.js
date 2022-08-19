0b94fb9119e4bdfec3594590f1e284747f268ada
telemahos <kakoulis@hotmail.com>
2022-08-19 21:46:05 +0300

    installed axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 import CoreuiVue from '@coreui/vue'
 import CIcon from '@coreui/icons-vue'
@@ -11,7 +12,7 @@ import DocsExample from '@/components/DocsExample'
 
 const app = createApp(App)
 app.use(store)
-app.use(router)
+app.use(router, axios)
 app.use(CoreuiVue)
 app.provide('icons', icons)
 app.component('CIcon', CIcon)
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,6 +4,11 @@ export default createStore({
   state: {
     sidebarVisible: '',
     sidebarUnfoldable: false,
+    isAuthenticated: false,
+    token: '',
+    isLoading: false,
+    apiURL: 'http://127.0.0.1:8000/api',
+    today: '',
   },
   mutations: {
     toggleSidebar(state) {
@@ -14,6 +19,38 @@ export default createStore({
     },
     updateSidebarVisible(state, payload) {
       state.sidebarVisible = payload.value
+    },
+    initializeApp(state) {
+      if (localStorage.getItem('token')) {
+          state.token = localStorage.getItem('token')
+          state.isAuthenticated = true
+          state.today = new Date()
+          console.log("initializeApp Token", state.token)
+          console.log("initializeApp TODAY: ", state.today)
+          console.log("initializeApp isAuthenticated", state.isAuthenticated)
+      } else {
+          state.token = ''
+          state.isAuthenticated = false
+          // console.log("initializeApp Token", state.token)
+          console.log("initializeApp isAuthenticated", state.isAuthenticated)
+      }
+    },
+    setIsLoading(state, status) {
+      state.isLoading = status
+      console.log("setIsLoading")
+    },
+    setToken(state, token) {
+        state.token = token
+      state.isAuthenticated = true
+      // state.today = new Date()
+      // console.log("initializeApp TODAY22: ", state.token)
+        console.log("setToken")
+    },
+    removeToken(state) {
+        localStorage.setItem('token','')
+        state.token = ''
+        state.isAuthenticated = false
+        console.log("removeToken")
     },
   },
   actions: {},
